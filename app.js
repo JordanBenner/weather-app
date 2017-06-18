e0712a577ebf510d9f7bf373e79f866d82fdcde3
Jordan Benner <jordanbenner@outlook.com>
2017-06-18 10:28:51 -0500

    error

--- app.js
+++ app.js
@@ -99,6 +99,23 @@ function getcoordinates(position) {
         getWeather(CurrentWeatherURL, DailyForecastURL, "C", "m\/s")
     }
 
+    function showError(error) {
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            $("#weather").html("User denied the request for Geolocation.");
+            break;
+        case error.POSITION_UNAVAILABLE:
+            $("#weather").html("Location information is unavailable.");
+            break;
+        case error.TIMEOUT:
+            $("#weather").html("The request to get user location timed out.");
+            break;
+        case error.UNKNOWN_ERROR:
+            $("#weather").html("An unknown error occurred.");
+            break;
+    }
+}
+
 var PORT = process.env.PORT || 8000;
 
 app.listen(PORT, function(){
